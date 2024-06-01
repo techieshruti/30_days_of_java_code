957cbb099cbbe33f73ebc120f9bf99c05dc63a44
techieshruti <shrutichandra721@gmail.com>
2024-06-01 22:11:28 +0530

    Precise Formatter

---
+++ Day-1/PreciseFormat.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+//User function Template for Java
+
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        
+        System.out.print(result + " " );
+
+        System.out.printf("%.3f",result); 
+        
+    }    
+    
+}
+
+//{ Driver Code Starts.
+
+class PreciseFormat {
+	public static void main (String[] args) {
+		
+		//taking input using class Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int testcases = sc.nextInt();
+		
+		while(testcases-- > 0){
+		    
+		    //taking 2 variables a,b
+    		float a = sc.nextFloat();
+    		float b = sc.nextFloat();
+    		
+    		//creating an object of class Geeks
+    		Geeks g = new Geeks();
+    		
+    		//calling printInFormat() method
+    		//of class Geeks
+    		g.printInFormat(a, b);
+    		System.out.println();
+		
+		}
+		
+	}
+}
