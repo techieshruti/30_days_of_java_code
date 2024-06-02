2cb1b3ff2aa6c8242b4e93c88ec41b6b026bcbb7
techieshruti <shrutichandra721@gmail.com>
2024-06-02 15:41:18 +0530

    added new file

---
+++ Day-2/PositiveOrNegative.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PositiveOrNegative
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter any number : ");
+        float num = scn.nextFloat();
+
+        if(num>0)
+        {
+         System.out.println("Entered number is Positive.");   
+        }
+        else
+        {
+            System.out.println("Entered number is Negative.");
+        }
+    }
+}
