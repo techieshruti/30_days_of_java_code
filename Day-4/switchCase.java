79b370063f00aaa6cc21ed1f29689de39ba811db
techieshruti <shrutichandra721@gmail.com>
2024-06-04 21:39:28 +0530

    added new file

---
+++ Day-4/switchCase.java
@@ -0,0 +1,21 @@
+// If the user's choice is 1, calculate the area of the circle having the given radius(R).Else if the choice is 2, calculate the area of the rectangle with the given length(L) and breadth(B).
+
+import java.util.*;
+
+public static switchCase()
+{
+    class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        
+        if(choice == 1)
+        {
+            return (3.14*arr[0]*arr[0]);
+        }
+        else if(choice ==  2)
+        {
+            return(L*B);
+        }
+        
+     }
+}
+}
