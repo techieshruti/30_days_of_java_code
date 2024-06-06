eceae09d19f9c18de4e7914e830e2575cdc45c7a
techieshruti <shrutichandra721@gmail.com>
2024-06-06 21:08:54 +0530

    added new file

---
+++ Day-6/halfPyramid.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class halfPyramid
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the number of lines you want to print : ");
+        int num = scn.nextInt();
+        
+        for(int i=1; i<=num; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
