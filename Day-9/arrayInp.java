1d9d8bce9789e749a97ee00013eeef4244a814af
techieshruti <shrutichandra721@gmail.com>
2024-06-10 12:31:08 +0530

    Taking input and displaying output using arrays

--- Day-9/arrayInp.java
+++ Day-9/arrayInp.java
@@ -6,20 +6,22 @@ public class arrayInp
 {
     public static void main(String[] args)
     {
-        Scanner scn = new Scanner(System.in);
-        int marks[] = new int[5];
-        int rollno[] = new int[5];
+        Scanner scn = new Scanner(System.in);  //creating object of scanner class
+        int marks[] = new int[5];              //initialized array
+        int rollno[] = new int[5];             //initialized array
 
         System.out.println("Enter full name of an student : ");
-        String name = scn.nextLine();
+        String name = scn.nextLine();          //Taking input from user
         System.out.println("Enter Roll No : ");
-        rollno[0] = scn.nextInt();
+        rollno[0] = scn.nextInt();             //Taking input from user
         System.out.println("Enter marks of Maths : ");
-        marks[0] = scn.nextInt();
+        marks[0] = scn.nextInt();              //Taking input from user
         System.out.println("Enter marks of Physics : ");
-        marks[1] = scn.nextInt();
+        marks[1] = scn.nextInt();              //Taking input from user
         System.out.println("Enter marks of Chemistry : ");
-        marks[2] = scn.nextInt();
+        marks[2] = scn.nextInt();              //Taking input from user
 
-       }
+        int percent = (marks[0]+marks[1]+marks[2])/3;
+        System.out.println("Percentage is : "+percent);
+        }
 }
