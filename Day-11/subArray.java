d992fc9331d249a3d27e998c783872d59eb33cde
techieshruti <shrutichandra721@gmail.com>
2024-06-20 13:00:11 +0530

    Java program to print sub arrays of array

--- Day-11/subArray.java
+++ Day-11/subArray.java
@@ -6,7 +6,7 @@ public class subArray
 {
     public static void main(String[] args)
     {
-        int arr[] = {2,4,6,8,10,12,14};
+        int arr[] = {2,4,6,8,10};
         System.out.println("\n Given Array is :");
         for(int i=0; i<arr.length; i++)
         {
@@ -15,23 +15,33 @@ public class subArray
         System.out.println();
 
         subarrays(arr);
+        
     }
 
     public static void subarrays(int arr[])
     {
-        for(int i=0; i<arr.length; i++)
-        {
-            int start=i;
-            for(int j=0; j<arr.length; j++)
+        int ts =0;
+        List<Integer> subArraySums = new ArrayList<>();
+       for(int i=0; i<arr.length; i++)
+       {
+         int start=i;
+         for(int j=i; j<arr.length; j++)
+         {
+            int end = j;
+            int sum = 0;
+            for(int k=start; k<=end; k++)
             {
-                int end=j;
-                for(int k=start; k<=end; k++)
-                {
-                    System.out.print(arr[k]+" ");
-                }
-                System.out.println();
+                System.out.print(" "+ arr[k]);
+                sum= sum+arr[k];
             }
+            subArraySums.add(sum);
+            ts++;
             System.out.println();
-        }
+         }
+         System.out.println();
+         
+       }
+       System.out.println("total subarrays are :" +ts);
+       System.out.println("sum of subarrays are :" +subArraySums);
     }
 }
