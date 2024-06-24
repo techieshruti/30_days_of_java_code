4f47abfd33aa26f44cfee0456bc10340f603d8ae
techieshruti <shrutichandra721@gmail.com>
2024-06-24 22:19:51 +0530

    java program to find second smallest number in array

--- Day-14/secondSmallestNum.java
+++ Day-14/secondSmallestNum.java
@@ -1,7 +1,9 @@
-public class secondSmallestNum{  
-public static int getSecondSmallest(int[] a, int total){  
-int temp;  
-for (int i = 0; i < total; i++)   
+public class secondSmallestNum
+{  
+    public static int getSecondSmallest(int[] a, int total)
+    {  
+        int temp;  
+        for (int i = 0; i < total; i++)   
         {  
             for (int j = i + 1; j < total; j++)   
             {  
@@ -13,11 +15,13 @@ for (int i = 0; i < total; i++)
                 }  
             }  
         }  
-       return a[1];//2nd element because index starts from 0  
+       return a[1]; 
 }  
-public static void main(String args[]){  
-int a[]={1,2,5,6,3,2};  
-int b[]={44,66,99,77,33,22,55};  
-System.out.println("Second smallest: "+getSecondSmallest(a,6));  
-System.out.println("Second smallest: "+getSecondSmallest(b,7));  
-}}  
+public static void main(String args[])
+{  
+   int a[]={1,2,5,6,3,2};  
+   int b[]={44,66,99,77,33,22,55};  
+   System.out.println("Second smallest: "+getSecondSmallest(a,6));  
+   System.out.println("Second smallest: "+getSecondSmallest(b,7));  
+}
+}  
