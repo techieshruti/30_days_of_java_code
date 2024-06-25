af94706a7df6cc339ebc12046744c4544c0713fe
techieshruti <shrutichandra721@gmail.com>
2024-06-25 19:04:44 +0530

    check if string is palindrome or not

--- Day-15/palindrome.java
+++ Day-15/palindrome.java
@@ -8,8 +8,9 @@ public class palindrome
     {
         for(int i=0; i<str.length()/2; i++) 
         {
-            int n = str.length()/2;
-            if(str.charAt(i) != str.charAt(n-1-i))
+            int low = 0;
+            int high = str.length()-1;
+            if(str.charAt(low) != str.charAt(high))
             {
                 return false;
             }
@@ -22,6 +23,6 @@ public class palindrome
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter any string to check if it is palindrome or not : ");
         String str = scn.nextLine();
-        System.out.println(isPalindrome(str));
+        System.out.println(str + " Is palindrome ? "+ isPalindrome(str));
     }
 }
