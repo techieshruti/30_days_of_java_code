02dabd2649724daad4b239f1bd58e013e1ee10ef
techieshruti <shrutichandra721@gmail.com>
2024-06-25 18:26:56 +0530

    charAt function

--- Day-15/stringLength.java
+++ Day-15/stringLength.java
@@ -4,11 +4,21 @@ import java.util.*;
 
 public class stringLength
 {
+    public static void printLetter(String str)
+    {
+        for(int i=0; i<str.length(); i++)
+        {
+            System.out.println(str.charAt(i)+ " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args)
     {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter your name : ");
         String str = scn.nextLine();
         System.out.println("Length of your name is : "+ str.length());
+        printLetter(str);
     }
 }
